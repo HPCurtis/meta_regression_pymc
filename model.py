82587a00d9406d3ff3386ea3e15b120fe98dd722
Harrison <harrisoncurtis50@gmail.com>
2024-09-27 17:01:48 +0100

    Fix indent error for funtion alignment in the script and add N parameter to qr_decomp_scale functin

--- model.py
+++ model.py
@@ -21,7 +21,7 @@ def meta_reg(X, y):
     """
     # Get the len of the y variable
     N = len(y) 
-    Q,R,R_inverse = qr_decomp_scale(X)
+    Q,R,R_inverse = qr_decomp_scale(X, N)
 
     # Define the pymc model for linear regression with qr decomp based heavily on 
     # https://mc-stan.org/docs/stan-users-guide/regression.html#QR-reparameterization.section
@@ -44,22 +44,22 @@ def meta_reg(X, y):
         return trace, model
 
 
-    def qr_decomp_scale(X):
-        try:
-            # Calculate the thin qr decompositon and the inverse of upper-triangular matrix R (defualt fucntion behaviour).
-            Q,R = qr(X)
-            # Scale Q and R.
-            Q = Q * sqrt(N - 1)
-            R = R / sqrt(N - 1)
-            # Calculate the inverse of the upper triangulr scaled R matrix from qr_decompisition. 
-            R_inverse = inv(R)
-            return Q,R,R_inverse
-        
-        # Handle error when matrxi is not presneted
-        except LinAlgError:
-            sys.exit("Input to qr_decomp_scale is not a matrix") 
-        except ValueError:
-            sys.exit()
+def qr_decomp_scale(X, N):
+    try:
+        # Calculate the thin qr decompositon and the inverse of upper-triangular matrix R (defualt fucntion behaviour).
+        Q,R = qr(X)
+        # Scale Q and R.
+        Q = Q * sqrt(N - 1)
+        R = R / sqrt(N - 1)
+        # Calculate the inverse of the upper triangular scaled R matrix from qr_decompisition. 
+        R_inverse = inv(R)
+        return Q,R,R_inverse
+    
+    # Handle error when matrxi is not presneted
+    except LinAlgError:
+        sys.exit("Input to qr_decomp_scale is not a matrix") 
+    except ValueError:
+        sys.exit()
 
 # Define main fucntion for testing of model.
 def main():
